fcbfdeb484f3d2fd9820798ffcbb7612a6aa661a
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-05-29 20:32:31 +0530

    modify get posts controller to show comment author

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,13 +1,21 @@
 const asyncHandler = require("express-async-handler");
 const Post = require("../models/post");
-const User = require("../models/user");
-const Comment = require("../models/comment");
+const User = require("../models/user"); // Import User model
+const Comment = require("../models/comment"); // Import Comment model
 
 exports.posts = asyncHandler(async (req, res, next) => {
+  // Find all posts and populate 'author' and 'comments'
   const allPosts = await Post.find({})
     .populate("author", "username")
-
+    .populate({
+      path: "comments",
+      populate: {
+        path: "author",
+        select: "username",
+      },
+    })
     .exec();
 
+  // Send the response as JSON
   res.json(allPosts);
 });
