5f114fadabb8e04300077e34eacbb613a6fc1a05
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-05-29 19:21:14 +0530

    update user  model schema
    to make usernames 'unique'

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   firstname: { type: String, required: true },
   password: { type: String, required: true },
 });
