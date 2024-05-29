e8941cb50e73d0117906f02f3ac576a3926ae499
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-05-29 19:20:20 +0530

    update posts models
    To handle multiple comments
    To handle default date

--- models/post.js
+++ models/post.js
@@ -5,10 +5,10 @@ const { Schema } = mongoose;
 const PostSchema = new Schema({
   title: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  date: { type: Date, required: true },
+  date: { type: Date, default: Date.now, required: true },
   text: { type: String, required: true },
   status: { type: String, required: true, enum: ["Public", "Private"] },
-  comments: { type: Schema.Types.ObjectId, ref: "Comments" },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
 });
 
 module.exports = mongoose.model("Post", PostSchema);
