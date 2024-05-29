2ab2bc3f489f01a1b8d88efe12236ee2835824f6
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-05-29 19:13:03 +0530

    add user model schema

--- models/post.js
+++ models/post.js
@@ -7,7 +7,8 @@ const PostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   date: { type: Date, required: true },
   text: { type: String, required: true },
-  comments: { type: Schema.Types.ObjectId, red: "Comments" },
+  status: { type: String, required: true, enum: ["Public", "Private"] },
+  comments: { type: Schema.Types.ObjectId, ref: "Comments" },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
