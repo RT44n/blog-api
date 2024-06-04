f1c82d42ce0b9e65b448570e74d8e5dc3b5eaae0
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-06-04 17:26:07 +0530

    fix import error

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
 // Local Strategy
 passport.use(
